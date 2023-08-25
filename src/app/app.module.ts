0e4539742d22cf3ce080f749c63d71daffb6c9ad
mahesh <gmahesh8184@gmail.com>
2023-08-25 10:55:30 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ChatService } from './chat.service';
+import { AuthService } from './auth.service';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './chat/chat.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  //  LoginComponent,
+//ChatComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+    FormsModule,
+    
+  ],
+  providers: [ChatService,AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
